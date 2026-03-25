d8ebc0f6fbe5cdbb8937bd15b62998a9b37a8245
Mac <maclong9@icloud.com>
2026-03-25 18:24:24 +0100

    chore: replace local path dependencies with GitHub URLs

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "ScorePostgres", targets: ["ScorePostgres"]),
     ],
     dependencies: [
-        .package(path: "../../score"),
+        .package(url: "https://github.com/allegro-systems/score.git", branch: "main"),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.21.0"),
     ],
     targets: [
